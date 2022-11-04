f6b38300ad739e9e19614a737b40389b094d5d92
lucaspaulii <lucaspauli22@gmail.com>
2022-11-04 00:44:21 -0300

    sign-up post done and tested with an aux get (to be removed later) also tweets post made (untested)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,37 @@
-import express from {express};
-import cors from {cors};
+import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(express.json());
+
+const users = [];
+const tweets = [];
+
+app.post("/sign-up", (req, res) => {
+  const { username, avatar } = req.body;
+  const newUser = {
+    username,
+    avatar,
+  };
+  users.push(newUser);
+  res.status(200).send("OK");
+});
+
+app.get("/sign-up", (req, res) => {
+    res.send(users)
+})
+
+app.post("/tweets", (req, res) => {
+    const {username, tweet} = req.body
+    const newTweet = {
+        username,
+        tweet
+    }
+    tweets.push(tweet)
+    res.status(200).send("OK");
+})
+
+app.get("/tweets", (req, res) => {
+})
 
 app.listen(5000);
